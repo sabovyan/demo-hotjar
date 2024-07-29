4ba941176b30acd0188eaa8722d568f23f6f3b49
sabovyan <abovyan.sargis90@gmail.com>
2024-07-29 09:26:55 +0400

    handle hotjar button click

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,32 @@
 "use client";
 
 import Hotjar from "@hotjar/browser";
-import { Suspense } from "react";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const siteId = process.env.NEXT_PUBLIC_HOT_JAR_SITE_ID;
+    const HOT_JAR_VERSION = 6;
+
+    if (siteId) {
+      Hotjar.init(Number(siteId), HOT_JAR_VERSION, {
+        debug: true,
+      });
+    } else {
+      console.error("site id not found");
+    }
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-24">
       <button
         className="border border-black rounded p-2"
         onClick={() => {
-          const siteId = process.env.NEXT_PUBLIC_HOT_JAR_SITE_ID;
-          const HOT_JAR_VERSION = 6;
+          const hotjarButton = document.querySelector<HTMLButtonElement>(
+            "#_hj_feedback_container button",
+          );
 
-          if (siteId) {
-            Hotjar.init(Number(siteId), HOT_JAR_VERSION, {
-              debug: true,
-            });
-          } else {
-            console.error("site id not found");
-          }
+          hotjarButton && hotjarButton.click();
         }}
       >
         try hot jar
